6b968420887cf14df1dc105894a1275bd05dbcfd
dddraxxx <dongqh078@gmail.com>
2025-06-02 18:19:40 +0000

    Refactor global_install.sh to streamline pip installation commands using a helper function

--- global_install.sh
+++ global_install.sh
@@ -5,20 +5,24 @@ if ! command -v uv &> /dev/null; then
     source $HOME/.local/bin/env
 fi
 
-uv pip install -e .[math,geo,vllm] --system
-uv pip install flash-attn --no-build-isolation --system
-uv pip install -e .[gpu] --system
-uv pip install pip --system
-uv pip install tensorboard --system
+uvp(){
+    uv pip install $@ --system
+}
+uvp -e .[math,geo,vllm]
+uvp flash-attn --no-build-isolation
+uvp -e .[gpu]
+uvp pip
+uvp tensorboard
+uvp nvitop
 
-uv pip install evaluate --system
-uv pip install transformers==4.51.3 --system
-uv pip install vllm==0.8.2 --system
-uv pip install -U pynvml --system
-uv pip install mathruler --system
-uv pip install pydantic --upgrade --system
-uv pip install openai --upgrade --system
-uv pip install tensordict==0.6.2 --system
-uv pip install triton==3.2.0 --system
-uv pip install qwen_vl_utils --system
-uv pip install math_verify --system
+uvp evaluate
+uvp transformers==4.51.3
+uvp vllm==0.8.2
+uvp -U pynvml
+uvp mathruler
+uvp pydantic --upgrade
+uvp openai --upgrade
+uvp tensordict==0.6.2
+uvp triton==3.2.0
+uvp qwen_vl_utils
+uvp math_verify
